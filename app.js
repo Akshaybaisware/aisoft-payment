e5d5a13c64a69655f66deea7bcc3a76c9cfa0f1b
akshaybaisware <akshaybaisware1998@gmail.com>
2024-07-30 15:48:11 +0530

    app.js changed

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+// import express, { json } from "express";
+// import cors from 'cors'
+// import dotenv from "dotenv"
+// import router from "./router/paymentRoute.js";
+
+// dotenv.config()
+// const app = express()
+
+// // const allowedOrigins = [   'http://localhost:5174' , 'http://localhost:3030','https://aisoft-payment-production.up.railway.app'];
+// const allowedOrigins = [ 'https://aisoftsolution.in'];
+// // app.use(cors({
+// //     origin: function (origin, callback) {
+// //         if (!origin || allowedOrigins.includes(origin)) {
+// //             callback(null, true);
+// //         } else {
+// //             callback(new Error('Not allowed by CORS'));
+// //         }
+// //     }
+// // }));
+
+// app.use(cors());
+
+// app.use(json());
+// app.use(express.urlencoded({ extended: true }));
+// app.set('view engine', 'ejs');
+
+// app.use('/', router)
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`http://localhost:3030`)
+// })
+
 import express, { json } from "express";
-import cors from 'cors'
-import dotenv from "dotenv"
+import cors from 'cors';
+import dotenv from "dotenv";
 import router from "./router/paymentRoute.js";
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-const allowedOrigins = ['http://localhost:3030','http://localhost:5174' , 'https://aisoft-payment-production.up.railway.app'];
+const allowedOrigins = ['https://aisoftsolution.in'];
+
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
@@ -21,8 +54,10 @@ app.use(json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.use('/', router)
+app.use('/', router);
 
 app.listen(process.env.PORT, () => {
-    console.log(`http://localhost:3030`)
-})
+    // console.log(http://localhost:${process.env.PORT});
+});
+
+    
